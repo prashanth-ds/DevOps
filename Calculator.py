761a1b7508518fda2abc211de46daa606884e698
prashanth-ds <prashanthds258@gmail.com>
2021-07-26 11:12:15 +0530

    init

---
+++ Calculator.py
@@ -0,0 +1,58 @@
+import operator
+
+
+class Calculator:
+
+    def __init__(self, expression):
+        self.exp = expression
+        self.operators = ['+', '-', '*']
+        self.operation = ''
+        self.integers = []
+        self.eval = {'+': operator.add, '-': operator.sub, '*': operator.mul}
+
+    def __str__(self):
+        # return f"Entered expression: {self.exp}"
+        return f"{self.integers}"
+
+    def __validate(self):
+        """
+        Here we validate the expression, where two positive integers and operators (+,-,*) are valid.
+        :return: Boolean : True for valid expression, and vice versa.
+        """
+
+        for op in self.operators:
+            if op in self.exp:
+                self.operation = op
+                try:
+                    self.integers = list(map(int, self.exp.split(self.operation)))
+                    if len(self.integers) == 2:
+                        return "Correct"
+                    elif len(self.integers) < 2:
+                        return "No Valid Expression"
+                    else:
+                        # control comes here if operator is same, as the split func will split according to initially
+                        # found operator and if length of self.integers is more than 2 ofcourse there are more than
+                        # two operands with same operator
+                        return "Same Operator Used More than once"
+                except ValueError:
+                    # control comes here when different operators are used, ofcourse more than 2 operands
+                    return "Operators Used Multiple times"
+
+    def evaluate(self):
+        """
+        This method will evaluate the validated expression
+        :return: The Evaluated Output.
+        """
+
+        condition = self.__validate()
+
+        if condition == "Correct":
+            return self.eval[self.operation](*self.integers)
+        else:
+            if condition is None:
+                return "Operator Invalid"
+            return condition
+
+
+# obj = Calculator("2/4")
+# print(obj.evaluate())
---
+++ Unit_Testing.py
@@ -0,0 +1,41 @@
+import unittest
+from Calculator import Calculator
+
+
+class UnitTesting(unittest.TestCase):
+
+    def setUp(self):
+        self.addition = Calculator("10+15")
+        self.subtraction = Calculator("10-5")
+        self.multiplication = Calculator("11*11")
+        self.multiple_operands = Calculator("1+2+3")
+        self.multiple_operators = Calculator("2+2*5")
+        self.invalid_operator = Calculator("2/2")
+
+    def test_add(self):
+        addition = self.addition.evaluate()
+        self.assertEqual(addition, 25)
+
+    def test_sub(self):
+        subtraction = self.subtraction.evaluate()
+        self.assertEqual(subtraction, 5)
+
+    def test_mul(self):
+        multiplication = self.multiplication.evaluate()
+        self.assertEqual(multiplication, 121)
+
+    def test_operands(self):
+        multiple_operands = self.multiple_operands.evaluate()
+        self.assertEqual(multiple_operands, 6)
+
+    def test_MultipleOperator(self):
+        multiple_operators = self.multiple_operators.evaluate()
+        self.assertEqual(multiple_operators, 12)
+
+    def test_operator(self):
+        invalid_operator = self.invalid_operator.evaluate()
+        self.assertEqual(invalid_operator, 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
